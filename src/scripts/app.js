f14f4e1e5d93dfbf44218103f8d36e6508dd5416
Douglas (Amarelo) <douglas.lopes_@hotmail.com>
2016-09-10 12:32:50 -0300

    Finalização
    Criação dos scripts de calculo
    Melhoria no layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,59 @@
 'use strict';
 
-//load modules
-require('app/helpers');
+var valorSalario     = $('#valor-salario'),
+	valorHora        = $('#valor-hora'),
+	horasTrabalhadas = $('#horas-trabalhadas'),
+	valorTotal       = $('.total__val'),
+	limpaCampos      = $('.form__limpar'),
+	calcular         = $('.form__calcular');
 
-// var login 		= require('app/login'),
-// 	video 		= require('app/video'),
-// 	forms 		= require('app/forms'),
-// 	share 		= require('app/share'),
-// 	accordion 	= require('app/accordion'),
-// 	lightbox 	= require('app/lightbox');
 
-// $(function() {
-// 	login();
-// 	video();
-// 	forms();
-// 	share();
-// 	accordion();
-// 	lightbox();
-// });
+var escreveValor = function(valor) {
+	valorTotal.text(valor);
+};
+
+var verificaCampo = function(campo) {
+	if (campo !== '') {
+		return campo.val();
+	}
+};
+
+var somaSalario = function() {
+	var valor = verificaCampo(valorHora);
+		valor = valor * 160;
+
+	valorSalario.val(valor / 2);
+};
+
+var somaValorHora = function() {
+	var valor = verificaCampo(valorSalario);
+		valor = valor / 160;
+
+	valorHora.val(valor * 2);
+};
+
+var somaHorasTrabalhadas = function() {
+	var valor = verificaCampo(horasTrabalhadas);
+
+	valor = valor * valorHora.val();
+
+	escreveValor(valor);
+};
+
+valorSalario.on('keyup change', function() {
+	somaValorHora();
+});
+
+horasTrabalhadas.on('keyup change', function() {
+	somaHorasTrabalhadas();
+});
+
+limpaCampos.click(function() {
+	valorTotal.text('0');
+});
+
+calcular.click(function(e) {
+	e.preventDefault();
+	somaHorasTrabalhadas();
+	somaSalario();
+});
