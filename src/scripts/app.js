981e03f0eb8706bcccd11e3df061d8c6fa93e05e
Lucas Monteverde <monteverde13@gmail.com>
2016-09-13 16:34:18 -0300

    code refactoring

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,55 +1,54 @@
 'use strict';
+
+var $ = require('app/helpers');
+
+var data = {};
+
+var updateDom = function() {
+	//console.log(data);
+	$('valor-salario').value = data.salary;
+	$('valor-hora').value = data.price;
+	$('valor-total').innerText = data.price * data.hour;
+};
+
+var handleSalary = function() {
+	data.salary = this.value;
+	data.price = (data.salary / 160 ) * 2;
+	updateDom();
+};
+
+var handlePrice = function() {
+	data.price = this.value;
+	data.salary = (data.price * 160 ) / 2;
+	updateDom();
+};
+
+var handleHours = function() {
+	data.hour = this.value;
+	updateDom();
+};
+
+var resetValues = function() {
+	data = {
+		salary: 0,
+		price: 0,
+		hour: 0
+	};
+
+	updateDom();
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	$('valor-salario').bind('keyup', handleSalary ).bind('change', handleSalary );
+	$('valor-hora').bind('keyup', handlePrice ).bind('change', handlePrice );
+	$('horas-trabalhadas').bind('keyup', handleHours ).bind('change', handleHours );
+
+	$('clear').bind('click', resetValues);
+	resetValues();
+});
+
+
 /*
-var valorSalario     = document.querySelector('#valor-salario'),
-	valorHora        = document.querySelector('#valor-hora'),
-	horasTrabalhadas = document.querySelector('#horas-trabalhadas'),
-	valorTotal       = document.querySelector('.total__val'),
-	limpaCampos      = document.querySelector('.form-limpar'),
-	calcular         = document.querySelector('.form-calcular');
-
-var somaSalario = function() {
-	var valor = valorHora.getAttribute('value') * 160;
-
-	valorSalario.setAttribute('value', valor / 2);
-};
-
-var somaValorHora = function() {
-	var valor = valorSalario.getAttribute('value') / 160;
-
-	valorHora.setAttribute('value', valor * 2);
-};
-
-var somaHorasTrabalhadas = function() {
-	var valor = horasTrabalhadas.getAttribute('value');
-
-	valor = valor * valorHora.getAttribute('value');
-
-	valorTotal.innerHTML = valor;
-};
-
-valorSalario.addEventListener('keyup', function() {
-	console.log(10);
-	somaValorHora();
-});
-
-horasTrabalhadas.addEventListener('keyup', function() {
-	console.log(20);
-	somaHorasTrabalhadas();
-	somaSalario();
-});
-
-limpaCampos.click(function() {
-	valorTotal.innerHTML = '0';
-});
-
-calcular.click(function() {
-	// e.preventDefault();
-	somaHorasTrabalhadas();
-	somaSalario();
-});
-*/
-
-
 var valorSalario     = $('#valor-salario'),
 	valorHora        = $('#valor-hora'),
 	horasTrabalhadas = $('#horas-trabalhadas'),
@@ -108,4 +107,4 @@ calcular.click(function(e) {
 	somaHorasTrabalhadas();
 	somaSalario();
 });
-
+*/
